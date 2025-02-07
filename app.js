b4cfda5d5aa308d0ef9ffe2952ccc77a2c3f4df8
Kathy Grace <156879423+gracelaine407@users.noreply.github.com>
2025-02-07 15:16:13 -0600

    JS functions update

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-// JavaScript Document - Variables
-// Assign the value 7 to a
-{let a = 7; //These are block variables
-// Assign the value 3 to b
-let b = 3;
-// Assign the value a + b to c:
-let c = a + b;} //This Performs a mathematical operation
+// JavaScript Document - Functions
+
 
 var Firstname = "Kathy" //This is a global variable and it is a string variable
 
 
 
 
-console.log("a=7, b=3, c=a+b");
+
 
 console.log(Firstname);
