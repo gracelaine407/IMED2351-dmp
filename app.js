c441caaaf77d6f1e343550b7ae44150ee535766b
Kathy Grace <156879423+gracelaine407@users.noreply.github.com>
2025-02-22 01:25:24 -0600

    Add files via upload
    new assignment files

--- app.js
+++ app.js
@@ -3,10 +3,22 @@
 
 var Firstname = "Kathy" //This is a global variable and it is a string variable
 
-var Assignment ="Assignment 4.5"
-
-
-
+var Assignment ="Assignment 5.4"
 
 console.log(Firstname);
 console.log(Assignment);
+
+let breedNumber = 1;
+
+while (breedNumber < 5) {
+    console.log("Number of breeds is 4");
+    breedNumber++;
+}
+
+const title = document.querySelector(".title");
+
+title.style.color= "#5e308b";
+title.style.backgroundColor = "white";
+
+
+const button = document.querySelector(".button");
