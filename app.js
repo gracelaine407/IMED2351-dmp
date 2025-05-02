762ab48a35e4b11fef90bb35e8e7b0f55d9c3186
Kathy Grace <156879423+gracelaine407@users.noreply.github.com>
2025-05-01 23:47:46 -0500

    Add files via upload
    final project files

--- app.js
+++ app.js
@@ -1,123 +1,127 @@
-// JavaScript Document - Rock, paper, scissors game
+//javaScript
 
-const game = ()=> {
-    let pScore = 0;
-    let cScore = 0;
 
-    //Start the Game
-    const startGame = () => {
-        const playBtn = document.querySelector(".intro button");
-        const introScreen = document.querySelector(".intro");
-        const match = document.querySelector(".match");
-    
-        playBtn.addEventListener("click", () => {
-          introScreen.classList.add("fadeOut");
-          match.classList.add("fadeIn");
-        });
-      };
+function loadDoc() {
+   const xhttp = new XMLHttpRequest();
+   xhttp.onload = function() {
+     document.getElementById("instructions").innerHTML =
+     this.responseText;
+   }
+   xhttp.open("GET", "GameInstructions.txt");
+   xhttp.send();
+ }
 
-      //Play Match
-      const playMatch = () => {
-        const options = document.querySelectorAll(".options button");
-        const playerHand = document.querySelector(".player-hand");
-        const computerHand = document.querySelector(".computer-hand");
-        const hands = document.querySelectorAll(".hands img");
-    
-        hands.forEach(hand => {
-          hand.addEventListener("animationend", function() {
-            this.style.animation = "";
-          });
-        });
-        //Computer Options
-        const computerOptions = ["rock", "paper", "scissors"];
-    
-        options.forEach(option => {
-          option.addEventListener("click", function() {
-           
-            //Computer Choice
-            const computerNumber = Math.floor(Math.random() * 3);
-            const computerChoice = computerOptions[computerNumber];
-    
-            setTimeout(() => {
-              //Here is where we call compare hands
-              compareHands(this.textContent, computerChoice);
-              
-              //Update Images
-              playerHand.src = `./assets/${this.textContent}.png`;
-              computerHand.src = `./assets/${computerChoice}.png`;
-            }, 2000);
-            //Animation
-            playerHand.style.animation = "shakePlayer 2s ease";
-            computerHand.style.animation = "shakeComputer 2s ease";
-          });
-        });
-      };
-    
-      const updateScore = () => {
-        const playerScore = document.querySelector(".player-score p");
-        const computerScore = document.querySelector(".computer-score p");
-        playerScore.textContent = pScore;
-        computerScore.textContent = cScore;
-      };
-    
-      const compareHands = (playerChoice, computerChoice) => {
-        //Update Text
-        const winner = document.querySelector(".winner");
+const section = document.querySelector('section');
+const playerLivesCount = document.querySelector('span');
+let playerLives = 6;
 
-        //Checking for a tie
-        if (playerChoice === computerChoice) {
-          winner.textContent = "It's a tie";
-          return;
-        }
-        //Check for Rock
-        if (playerChoice === "rock") {
-          if (computerChoice === "scissors") {
-            winner.textContent = "Player Wins";
-            pScore++;
-            updateScore();
-            return;
-          } else {
-            winner.textContent = "Computer Wins";
-            cScore++;
-            updateScore();
-            return;
-          }
-        }
-        //Check for Paper
-        if (playerChoice === "paper") {
-          if (computerChoice === "rock") {
-            winner.textContent = "Player Wins";
-            pScore++;
-            updateScore();
-            return;
-          } else {
-            winner.textContent = "Computer Wins";
-            cScore++;
-            updateScore();
-            return;
-          }
-        }
-        //Check for Scissors
-        if (playerChoice === "scissors") {
-          if (computerChoice === "paper") {
-            winner.textContent = "Player Wins";
-            pScore++;
-            updateScore();
-            return;
-          } else {
-            winner.textContent = "Computer Wins";
-            cScore++;
-            updateScore();
-            return;
-          }
-        }
-      };
-    
-      //Call all the Inner Functions
-      startGame();
-      playMatch();
-    };
-    
-    //Start the Game Function
-    game();
-    
+
+//link text
+playerLivesCount.textContent = playerLives;
+
+//generate data
+const getData = () => [
+  {name: 'bunny1', imgSrc: './images/bunny1.jpg'},
+  {name: 'bunny2', imgSrc: './images/bunny2.jpg'},
+  {name: 'bunny3', imgSrc: './images/bunny3.jpg'},
+  {name: 'bunny1', imgSrc: './images/bunny1.jpg'},
+  {name: 'bunny2', imgSrc: './images/bunny2.jpg'},
+  {name: 'bunny3', imgSrc: './images/bunny3.jpg'}
+];
+
+//shuffle card data
+const randomize = () => {
+  const cardData = getData();
+  cardData.sort(() => Math.random() - 0.5);
+  return cardData;
+};
+
+//generate cards
+const cardGenerator = () => {
+  const cardData = randomize();
+  //html
+  cardData.forEach((item) => {
+  const card = document.createElement('div');
+  const face = document.createElement('img');
+  const back = document.createElement('div');
+  card.classList = 'card';
+  face.classList = 'face';
+  back.classList = 'back';
+
+  face.src = item.imgSrc;
+  card.setAttribute('name', item.name);
+
+
+  section.appendChild(card);
+  card.appendChild(face);
+  card.appendChild(back);
+
+  card.addEventListener('click', (e) => {
+    card.classList.toggle('toggleCard');
+    checkCards(e);
+  })
+  });
+};
+//check for match
+const checkCards = (e) => {
+  const clickedCard = e.target;
+  clickedCard.classList.add('flipped');
+  const flippedCards = document.querySelectorAll('.flipped');
+  const toggleCard = document.querySelectorAll('.toggleCard');
+
+  if (flippedCards.length === 2) {
+    if (
+      flippedCards[0].getAttribute('name') ===
+      flippedCards[1].getAttribute('name')
+    ) {
+      console.log('match');
+      flippedCards.forEach((card) => {
+        card.classList.remove('flipped');
+        card.style.pointerEvents = 'none';
+      });
+    } else {
+      console.log('wrong');
+      flippedCards.forEach((card) => {
+        card.classList.remove('flipped');
+        setTimeout(() => card.classList.remove('toggleCard'), 1000);
+      });
+      playerLives--;
+      playerLivesCount.textContent = playerLives;
+      if(playerLives === 0) {
+        restart('You lost.  Try again!');
+      }
+    }
+  }
+  //check for win
+  if (toggleCard.length === 6) {
+    restart('Congratulations!  You won.');
+  }
+};
+
+//restart
+const restart = (text) => {
+  let cardData = randomize();
+  let faces = document.querySelectorAll('.face');
+  let cards = document.querySelectorAll('.card');
+  section.style.pointerEvents = 'none';
+  cardData.forEach((item, index) => {
+    cards[index].classList.remove('toggleCard');
+
+    setTimeout(() => {
+      cards[index].style.pointerEvents = 'all';
+    faces[index].src = item.imgSrc;
+    cards[index].setAttribute('name', item.name);
+    section.style.pointerEvents = 'all';
+    },  1000);
+  });
+  playerLives = 6;
+  playerLivesCount.textConte = playerLives;
+  setTimeout(() => window.alert(text), 500);
+};
+
+
+
+cardGenerator();
+
+
+  
